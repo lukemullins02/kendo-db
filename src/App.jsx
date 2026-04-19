fd7b88ef7ed22b401624ed2a4407958f7acc0f03
= <lukejmullins@gmail.com>
2026-04-19 15:54:47 -0400

    Add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Button } from "@progress/kendo-react-buttons";
 import Form from "./components/Form";
 import { useState } from "react";
 import { Dialog } from "@progress/kendo-react-dialogs";
+import { filterBy } from "@progress/kendo-data-query";
+import { TextBox } from "@progress/kendo-react-inputs";
 import "./App.css";
 
 const sampleClaims = [
@@ -30,6 +32,7 @@ export default function App() {
   const [claim, setClaim] = useState([...sampleClaims]);
   const [visibleDialog, setVisibleDialog] = useState(false);
   const [selectedClaim, setSelectedClaim] = useState(null);
+  const [filter, setFilter] = useState(null);
 
   const toggleDialog = () => {
     setVisibleDialog(!visibleDialog);
@@ -39,6 +42,8 @@ export default function App() {
     setSelectedClaim(e.dataItem);
     setVisibleDialog(true);
   };
+
+  const filteredClaims = filterBy(claim, filter);
 
   return (
     <div style={{ padding: 20 }}>
@@ -76,11 +81,33 @@ export default function App() {
           </Dialog>
         </>
       )}
+      <div style={{ marginBottom: "10px" }}>
+        <TextBox
+          placeholder="Search claims..."
+          onChange={(e) => {
+            const value = e.target.value;
 
+            setFilter(
+              value
+                ? {
+                    logic: "or",
+                    filters: [
+                      { field: "id", operator: "contains", value },
+                      { field: "name", operator: "contains", value },
+                      { field: "type", operator: "contains", value },
+                      { field: "status", operator: "contains", value },
+                      { field: "desc", operator: "contains", value },
+                    ],
+                  }
+                : null,
+            );
+          }}
+        />
+      </div>
       <Grid
         style={{ cursor: "pointer" }}
         onRowClick={handleRowClick}
-        data={claim}
+        data={filteredClaims}
       >
         <GridColumn field="id" title="Claim ID" />
         <GridColumn field="name" title="Name" />
